820f868319f2d0cdf860f8b5fa17fe76844bd409
Lucas Still <lucas.still@moduscreate.com>
2019-09-06 13:50:40 -0400

    task: Remove /main from url

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'main' },
   {
+    path: '',
+    pathMatch: 'full',
     loadChildren: () =>
       import('../views/main/main.module').then((m) => m.MainPageModule),
-    path: 'main',
   },
 ];
 
