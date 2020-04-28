99b477156b745f48441f56cc17a334cb823da038
raulmocian <raul.mocian@moduscreate.com>
2020-04-28 21:21:50 +0300

    Created a page object for the delete notification prompt

---
+++ e2e-wdio/page_objects/prompt.co.js
@@ -0,0 +1,32 @@
+class PromptCo {
+
+    get component(){ return $('//ion-toast');}
+
+    get notificationPrompt(){ return this.component.shadow$('.toast-header');}
+
+    get cancelButton() { return this.component.shadow$('.toast-button-group button:first-child');}
+
+    get dismissButton () { return this.component.shadow$('.toast-button-group button:nth-child(2)');}
+
+    promptDisplayed(){
+       return this.notificationPrompt.getText();
+    }
+
+    cancelBtnDisplayed(){
+        return this.cancelButton.getText();
+    }
+
+    dismissBtnDisplayed(){
+        return this.dismissButton.getText();
+    }
+
+    cancelBtnClick(){
+        this.cancelButton.click();
+    }
+
+    dismissBtnClick(){
+        this.dismissButton.click();
+    }
+
+}
+module.exports = new PromptCo();
